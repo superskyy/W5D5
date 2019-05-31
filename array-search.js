d50aac1bad6eccc57e29232bbffb58314ff670e9
Peter <psynevskyy.com>
2019-05-31 17:25:15 +0000

    refactored binary search

--- array-search.js
+++ array-search.js
@@ -1,13 +1,24 @@
-// Linear Search function
 function search(array, item) {
-  let index = null;
+  let min = 0;
+  let max = array.length - 1;
 
-  for (let i = 0; i < array.length; i++) {
-    if (item === array[i]) {
-      index = i;
-      break;
+  while (true) {
+    const middleIndex = Math.floor((min + max)/2.0);
+    const currentItem = array[middleIndex];
+
+    if (currentItem === item) {
+      // Found it
+      return middleIndex;
+    } else if (currentItem < item) {
+      // Look at the right side
+      min = middleIndex + 1;
+    } else {
+      // Look at the left side
+      max = middleIndex - 1;
+    }
+
+    if (min > max) {
+      return null;
     }
   }
-
-  return index;
 }
